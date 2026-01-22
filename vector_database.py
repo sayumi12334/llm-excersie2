d9edc816c2d4568e2327446b1aefe40b898ec466
Sayumi <buddikod@gmail.com>
2026-01-23 03:04:08 +0530

    code for populating and query of chromaDB

---
+++ vector_database.py
@@ -0,0 +1,34 @@
+# %%
+# populating for ChromaDB
+
+import chromadb
+
+ch_client = chromadb.Client()
+collection =ch_client.create_collection(name="test_collection")
+collection.add(
+   documents=[
+       "This is a document about BMI",
+       "This is a document about blood pressure ",
+       "This is a document about Complete Blood Count (CBC) "
+   ],
+   metadatas=[
+       {"source": "test 1"},
+       {"source": "test2"},
+       {"source": "test3"}
+   ],
+   ids=[
+       "b1",
+       "b2",
+       "b3"
+   ]
+)
+
+
+# %%
+# Query for ChromaDB
+results = collection.query(
+    query_texts=["This is a query about CBC and BMI"]  
+   n_results= 3)
+print(results)
+
+# %%
